5e596f7d7d223ec8979565ea1be6f6862f6c9234
Mallory Feuer <mallory.feuer@gmail.com>
2019-04-08 19:15:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function buildHistogram (arr) {
   let hist = {};
   for (let i = 0; i < arr.lengh; i++) {
     hist[arr[i]] = hist[arr[i]] + 1 || 1
+    
   }
-  return hist;
+  console.log(hist);
 }
